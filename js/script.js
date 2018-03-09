7dee1a97f286d9bc2ce0685455eb13da345e43cd
Hubert K <kolcz.h@gmail.com>
2018-03-09 23:42:19 +0100

    game_tab,5th-meeting

--- js/script.js
+++ js/script.js
@@ -6,4 +6,56 @@ $(window).scroll(function() {
       $('nav').removeClass('shrink');
     }
 });
+
+var tablica_gier = [
+    gra1 = {
+        nazwa: "PingPong",
+        link: "https://project-web-2018-01.github.io/PingPong/index.html"
+        //opis:
+        //ranking:
+        //obrazek:
+    },
+    gra2 = {
+        nazwa: "PingPong",
+        link: "https://project-web-2018-01.github.io/PingPong/index.html"
+        //opis:
+        //ranking:
+        //obrazek:
+    },
+    gra3 = {
+        nazwa: "PingPong",
+        link: "https://project-web-2018-01.github.io/PingPong/index.html"
+        //opis:
+        //ranking:
+        //obrazek:
+    },
+    gra4 = {
+        nazwa: "PingPong",
+        link: "https://project-web-2018-01.github.io/PingPong/index.html"
+        //opis:
+        //ranking:
+        //obrazek:
+    },
+    gra5 = {
+        nazwa: "PingPong",
+        link: "https://project-web-2018-01.github.io/PingPong/index.html"
+        //opis:
+        //ranking:
+        //obrazek:
+    },
+    gra6 = {
+        nazwa: "PingPong",
+        link: "mem"
+        //opis:
+        //ranking:
+        //obrazek:
+    }
+];
+
+$(".games-row > div").map(function(x,i,arr){ 
+    $(this).click(function() {
+        location.href = tablica_gier[x].link;
+    });
+});
+//console.log(tablica_gier[0]);
 });
